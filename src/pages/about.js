f5b0f510f87c39bb28cb39b724ae09de303aeef7
Billie Zhang <billiecoder@gmail.com>
2018-12-27 09:08:04 +0800

    show home page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Header from '../components/header'
 
 export default () => (
   <div>
-    <Header headerText="about" />
     <p>about page</p>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,82 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import Header from '../components/header'
+import { withStyles } from '@material-ui/core/styles'
+import Hidden from '@material-ui/core/Hidden'
+import Typography from '@material-ui/core/Typography'
+import { HEADER_HEIGHT, ACCENT_COLOR } from '../constants/GlobalStyle'
+import Hero from '../components/home/Hero'
+import CourseList from '../components/home/CourseList'
+import Footer from '../components/Footer'
+import Cat from '../components/home/Cat'
+import Layout from '../components/Layout'
 
-export default () => (
-  <div>
-    <Header />
-    <Button>gatsby</Button>
-    Hello gatsby!
-  </div>
+const styles = theme => ({
+  root: {
+    marginTop: HEADER_HEIGHT,
+    backgroundColor: '#ffffff'
+  },
+  content: {
+    padding: `${theme.spacing.unit * 6}px ${theme.spacing.unit * 2}px`,
+    width: '100%',
+    margin: '0 auto'
+  },
+  listWrap: {
+    backgroundColor: '#F2F7F4',
+    display: 'flex',
+    margin: '0 auto',
+    padding: '48px 24px',
+    [theme.breakpoints.up('md')]: {
+      width: 800
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: 1000
+    },
+    [theme.breakpoints.up('xl')]: {
+      width: 1100
+    }
+  },
+  text: {
+    padding: `${theme.spacing.unit * 10}px 0`,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  subheading: {
+    marginTop: theme.spacing.unit * 2
+  },
+  section: {
+    marginBottom: theme.spacing.unit * 4,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  line: {
+    width: 32,
+    borderBottom: `4px solid ${ACCENT_COLOR}`,
+    marginTop: theme.spacing.unit
+  }
+})
+
+const Home = ({ classes: s, courses }) => (
+  <Layout>
+    <div className={s.root}>
+      <Hidden smDown>
+        <Hero />
+      </Hidden>
+      <Cat />
+      <div className={s.content}>
+        <div className={s.section}>
+          <Typography variant="title">最新课程</Typography>
+          <div className={s.line} />
+        </div>{' '}
+        <div className={s.listWrap}>
+          <CourseList courses={courses || []} />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  </Layout>
 )
+
+export default withStyles(styles)(Home)
